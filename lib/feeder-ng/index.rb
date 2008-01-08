3585ed92f4b3fb5ecdd69be2d08ecaa63706add9
Ryan King <ryan@theryanking.com>
2008-01-08 14:25:32 -0800

    better offset and id updating

--- lib/feeder-ng/index.rb
+++ lib/feeder-ng/index.rb
@@ -23,11 +23,11 @@ module FeederNG
       t = Time.now
       l = data.length
       h = Digest::MD5.hexdigest(data)
-      o = @current_offset
+      o = @data_file.pos
 
       @data_file.write("#{i} #{t.xmlschema} #{o} #{l} #{h}\n")
       @data_file.write(data + "\n")
-      @last_id += 1
+      @last_id = @last_id + 1
 
       @index_file.write "#{i} #{t.xmlschema} #{o} #{l} #{h} 1\n"
       @index << {:id => i, :time => t, :offset => o, :length => l, :hash => h, :file => 1}
@@ -40,11 +40,11 @@ module FeederNG
       @data_file.seek i[:offset]
       header  = @data_file.readline
       content = @data_file.read(i[:length])
-      {:headers => parse_headers(header), :data => content}
+      [parse_headers(header), content]
     end
     
     def parse_headers str
-      m = str.match /\A(\d+) (\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}[+\-]\d{2}\:\d{2}) (\d+) (\d+) ([a-f0-9]+)\Z/
+      m = str.match(/\A(\d+) (\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}[+\-]\d{2}\:\d{2}) (\d+) (\d+) ([a-f0-9]+)\Z/)
       {:id => m.captures[0].to_i, :time => m.captures[1], :offset => m.captures[2].to_i, :length => m.captures[3].to_i, :hash => m.captures[4]}
     end
   end

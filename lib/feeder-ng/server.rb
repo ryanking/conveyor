c5f8a6c3bf376ff941629eb872745a8a67b85cd1
Ryan King <ryan@theryanking.com>
2008-01-18 11:54:45 -0800

    some refactoring + Content-MD5 headers

--- lib/feeder-ng/server.rb
+++ lib/feeder-ng/server.rb
@@ -59,37 +59,38 @@ module FeederNG
               head["Location"] = "/channels/#{m.captures[0]}/#{id}"
             end
           end
-        elsif request.get? && m = request.path_match(%r{/channels/(.*)/(\d+)})
-          if @channels.key?(m.captures[0])
-            headers, content = @channels[m.captures[0]].get(m.captures[1].to_i)
-            if headers && content
-              response.start(200) do |head, out|
-                out.write content
-              end
+
+        elsif request.get?
+          headers = content = nil
+          if m = request.path_match(%r{/channels/(.*)/(\d+)})
+            if @channels.key?(m.captures[0])
+              headers, content = @channels[m.captures[0]].get(m.captures[1].to_i)
             end
-          end
-        elsif request.get? && m  = request.path_match(%r{/channels/(.*)})
-          if @channels.key?(m.captures[0])
-            params = Mongrel::HttpRequest.query_parse(request.params['QUERY_STRING'])
-            headers = content = nil
-            if params.key? 'next'
-              if params.key? 'group'
-                headers, content = @channels[m.captures[0]].get_next_by_group(params['group'])
-              else
-                headers, content = @channels[m.captures[0]].get_next
-              end
-              if headers && content
-                response.start(200) do |head, out|
-                  head['Content-Location'] = "/channels/#{m.captures[0]}/#{headers[:id]}"
-                  out.write content
+          elsif m = request.path_match(%r{/channels/(.*)})
+            if @channels.key?(m.captures[0])
+              params = Mongrel::HttpRequest.query_parse(request.params['QUERY_STRING'])
+              if params.key? 'next'
+                if params.key? 'group'
+                  headers, content = @channels[m.captures[0]].get_next_by_group(params['group'])
+                else
+                  headers, content = @channels[m.captures[0]].get_next
                 end
               end
             end
+          else
+            response.start(200) do |head, out|
+              out.write("fake!")
+            end
           end
-        else
-          response.start(200) do |head, out|
-            out.write("fake!")
+
+          if headers && content
+            response.start(200) do |head, out|
+              head['Content-Location'] = "/channels/#{m.captures[0]}/#{headers[:id]}"
+              head['Content-MD5'] = headers[:hash]
+              out.write content
+            end
           end
+          
         end
       end
     end

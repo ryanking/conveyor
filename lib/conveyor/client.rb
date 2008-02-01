6403664df90fc7602cce6ae97cc0d57f946ca1dd
ryan king <ryan@theryanking.com>
2008-02-01 14:29:27 -0800

    basic client impl

---
+++ lib/conveyor/client.rb
@@ -0,0 +1,35 @@
+require 'net/http'
+
+module Conveyor
+  class Client
+    def initialize host, port = 8888
+      @host = host
+      @port = port
+      connect!
+    end
+    
+    def connect!
+      @conn = Net::HTTP.start(@host, @port)
+    end
+
+    def create_channel channel_name
+      @conn.put("/channels/#{channel_name}", nil, {'Content-Type' => 'application/octet-stream'})
+    end
+
+    def post channel_name, content
+      @conn.post("/channels/#{channel_name}", content, {'Content-Type' => 'application/octet-stream', 'Date' => Time.now.to_s})
+    end
+    
+    def get channel_name, id
+      @conn.get("/channels/#{channel_name}/#{id}").body
+    end
+
+    def get_next channel_name, group=nil
+      if group
+        @conn.get("/channels/#{channel_name}?next&group=#{group}").body
+      else
+        @conn.get("/channels/#{channel_name}?next").body
+      end
+    end
+  end
+end

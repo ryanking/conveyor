35e2fb26d47159ca002fd14087e16afd24333504
ryan king <ryan@theryanking.com>
2008-10-25 20:14:35 -0700

    switching back to thin from mongrel

--- lib/conveyor/server.rb
+++ lib/conveyor/server.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require 'mongrel'
 require 'conveyor/channel'
 require 'fileutils'
 require 'json'
@@ -49,87 +48,85 @@ module Conveyor
       @logger.info msg
     end
 
-    def put env, m
-      if Channel.valid_channel_name?(m.captures[0])
-        if !@channels.key?(m.captures[0])
-          create_new_channel m.captures[0]
-          i "#{env["REMOTE_ADDR"]} PUT #{env["REQUEST_PATH"]} 201"
-          [201, {}, "created channel #{m.captures[0]}"]
+    def put request, channel
+      if Channel.valid_channel_name?(channel)
+        if !@channels.key?(channel)
+          create_new_channel channel
+          i "#{request.env["REMOTE_ADDR"]} PUT #{request.fullpath} 201"
+          [201, {}, "created channel #{channel}"]
         else
-          i "#{env["REMOTE_ADDR"]} PUT #{env["REQUEST_PATH"]} 202"
+          i "#{request.env["REMOTE_ADDR"]} PUT #{request.fullpath} 202"
           [202, {}, "channel already exists. didn't do anything"]
         end
       else
-        i "#{env["REMOTE_ADDR"]} GET #{env["REQUEST_PATH"]} 406"
-        [406, {}, "invalid channel name. must match #{Channel::NAME_PATTERN}"]
+        i "#{request.env["REMOTE_ADDR"]} GET #{request.fullpath} 406"
+        [406, {}, "invalid channel name (#{channel}). must match #{Channel::NAME_PATTERN}"]
       end
     end
 
-    def post env, m
-      if @channels.key?(m.captures[0])
-        params = Mongrel::HttpRequest.query_parse(env['QUERY_STRING'])
-        if params.key?('rewind_id')
-          if params['group']
-            @channels[m.captures[0]].rewind(:id => params['rewind_id'], :group => params['group']).to_i # TODO make sure this is an integer
-            [200, {}, "iterator rewound to #{params['rewind_id']}"]
+    def post request, channel
+      if @channels.key?(channel)
+        if request.params.key?('rewind_id')
+          if request.params['group']
+            @channels[channel].rewind(:id => request.params['rewind_id'], :group => request.params['group']).to_i # TODO make sure this is an integer
+            [200, {}, "iterator rewound to #{request.params['rewind_id']}"]
           else
-            @channels[m.captures[0]].rewind(:id => params['rewind_id']).to_i # TODO make sure this is an integer
-            [200, {}, "iterator rewound to #{params['rewind_id']}"]
+            @channels[channel].rewind(:id => request.params['rewind_id']).to_i # TODO make sure this is an integer
+            [200, {}, "iterator rewound to #{request.params['rewind_id']}"]
           end
-        elsif params.key?('rewind_time')
-          if params['group']
-            @channels[m.captures[0]].rewind(:time => params['rewind_time'].to_i, :group => params['group']).to_i # TODO make sure this is an integer
-            [200, {}, "iterator rewound to #{params['rewind_id']}"]
+        elsif request.params.key?('rewind_time')
+          if request.params['group']
+            @channels[channel].rewind(:time => request.params['rewind_time'].to_i, :group => request.params['group']).to_i # TODO make sure this is an integer
+            [200, {}, "iterator rewound to #{request.params['rewind_id']}"]
           else
-            @channels[m.captures[0]].rewind(:time => params['rewind_time'].to_i) # TODO make sure this is an integer
-            [200, {}, "iterator rewound to #{params['rewind_time']}"]
+            @channels[channel].rewind(:time => request.params['rewind_time'].to_i) # TODO make sure this is an integer
+            [200, {}, "iterator rewound to #{request.params['rewind_time']}"]
           end
         else
-          if env.key?('HTTP_DATE') && d = Time.parse(env['HTTP_DATE'])
-            id = @channels[m.captures[0]].post(env['rack.input'].read)
-            i "#{env["REMOTE_ADDR"]} POST #{env["REQUEST_PATH"]} 202"
-            [202, {"Location" => "/channels/#{m.captures[0]}/#{id}"}, ""]
+          if request.env.key?('HTTP_DATE') && d = Time.parse(request.env['HTTP_DATE'])
+            id = @channels[channel].post(request.env['rack.input'].read)
+            i "#{request.env["REMOTE_ADDR"]} POST #{request.env["REQUEST_PATH"]} 202"
+            [202, {"Location" => "/channels/#{channel}/#{id}"}, ""]
           else
-            i "#{env["REMOTE_ADDR"]} POST #{env["REQUEST_PATH"]} 400"
+            i "#{request.env["REMOTE_ADDR"]} POST #{request.env["REQUEST_PATH"]} 400"
             [400, {}, "A valid Date header is required for all POSTs."]
           end
         end
-      elsif Channel.valid_channel_name?(m.captures[0])
-        create_new_channel(m.captures[0])
-        post(env, m)
+      elsif Channel.valid_channel_name?(channel)
+        create_new_channel(channel)
+        post(request, channel)
       else
         i "#{env["REMOTE_ADDR"]} #{env["REQUEST_METHOD"]} #{env["REQUEST_PATH"]} 404"
         [404, {}, '']
       end
     end
 
-    def get env
+    def get request
       headers = content = nil
-      if m = path_match(env, %r{/channels/(.*)/(\d+)})
+      if m = request.path_info.match(%r{/(.*)/(\d+)})
         if @channels.key?(m.captures[0])
           headers, content = @channels[m.captures[0]].get(m.captures[1].to_i)
         end
-      elsif m = path_match(env, %r{/channels/(.*)})
+      elsif m = request.path_info.match(%r{/(.*)})
         if @channels.key?(m.captures[0])
-          params = Mongrel::HttpRequest.query_parse(env['QUERY_STRING'])
-          if params.key? 'next'
-            if params.key? 'group'
-              if params.key? 'n'
-                list = @channels[m.captures[0]].get_next_n_by_group(params['n'].to_i, params['group'])
+          if request.params.key? 'next'
+            if request.params.key? 'group'
+              if request.params.key? 'n'
+                list = @channels[m.captures[0]].get_next_n_by_group(request.params['n'].to_i, request.params['group'])
               else
-                headers, content = @channels[m.captures[0]].get_next_by_group(params['group'])
+                headers, content = @channels[m.captures[0]].get_next_by_group(request.params['group'])
               end
             else
-              if params.key? 'n'
-                list = @channels[m.captures[0]].get_next_n(params['n'].to_i).map do |i|
+              if request.params.key? 'n'
+                list = @channels[m.captures[0]].get_next_n(request.params['n'].to_i).map do |i|
                   {:data => i[1], :hash => i[0][:hash], :id => i[0][:id]}
                 end
               else
                 headers, content = @channels[m.captures[0]].get_next
               end
             end
-          elsif params.key? 'after'
-            headers, content = @channels[m.captures[0]].get_nearest_after_timestamp(params['after'].to_i)
+          elsif request.params.key? 'after'
+            headers, content = @channels[m.captures[0]].get_nearest_after_timestamp(request.params['after'].to_i)
           else
             return [200, {}, @channels[m.captures[0]].status.to_json]
           end
@@ -139,30 +136,30 @@ module Conveyor
       end
 
       if headers && content
-        i "#{env["REMOTE_ADDR"]} GET #{env["REQUEST_PATH"]} 200 #{headers[:id]} #{headers[:length]} #{headers[:hash]}"
+        i "#{request.env["REMOTE_ADDR"]} GET #{request.env["REQUEST_PATH"]} 200 #{headers[:id]} #{headers[:length]} #{headers[:hash]}"
         return [
           200,
           {
-            'Content-Location' => "/channels/#{m.captures[0]}/#{headers[:id]}",
+            'Content-Location' => "/channels/#{m.captures.first}/#{headers[:id]}",
             'Content-MD5'      => headers[:hash],
             'Content-Type'     => 'application/octet-stream',
             'Last-Modified'    => Time.at(headers[:time]).gmtime.to_s,
           },
-            content
+          content
         ]
       elsif list
         return [200, {}, list.to_json]
       end
 
-      i "#{env["REMOTE_ADDR"]} #{env["REQUEST_METHOD"]} #{env["REQUEST_PATH"]} 404"
+      i "#{request.env["REMOTE_ADDR"]} #{request.env["REQUEST_METHOD"]} #{request.fullpath} 404"
       return [404, {}, '']
     end
 
-    def delete env, m
-      if @channels.key?(m.captures[0])
-        @channels[m.captures[0]].delete!
-        @channels.delete(m.captures[0])
-        [200, {}, "Channel deleted."]
+    def delete request, channel
+      if @channels.key?(channel)
+        @channels[channel].delete!
+        @channels.delete(channel)
+        [200, {}, "Channel '#{channel}' deleted."]
       end
     end
 
@@ -170,15 +167,16 @@ module Conveyor
       if @verbose
         pp env
       end
+      request = Rack::Request.new(env)
       @requests += 1
-      if env['REQUEST_METHOD']    == 'PUT'  && m = path_match(env, %r{/channels/(.*)})
-        put(env, m)
-      elsif env['REQUEST_METHOD'] == 'POST' && m = path_match(env, %r{/channels/(.*)})
-        post(env, m)
-      elsif @unsafe_mode && env['REQUEST_METHOD'] == 'DELETE' && m = path_match(env, %r{/channels/(.*)})
-        delete(env, m)
-      elsif env['REQUEST_METHOD'] == 'GET'
-        get(env)
+      if request.put?  && m = request.path_info.match(%r{/(.*)})
+        put(request, m.captures[0])
+      elsif request.post? && m = request.path_info.match(%r{/(.*)})
+        post(request, m.captures[0])
+      elsif @unsafe_mode && request.delete? && m = request.path_info.match(%r{/(.*)})
+        delete(request, m.captures[0])
+      elsif request.get?
+        get(request)
       else
         i "#{env["REMOTE_ADDR"]} #{env["REQUEST_METHOD"]} #{env["REQUEST_PATH"]} 404"
         [404, {}, '']
